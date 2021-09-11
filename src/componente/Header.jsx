908ad78b6f897124a7687f6dddaa6bce6381bc09
mrcomputer2018 <45561350+mrcomputer2018@users.noreply.github.com>
2021-09-11 08:08:49 -0300

    component header

---
+++ src/componente/Header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <div>
+            Haeder
+        </div>
+    )
+}
+export default Header;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,10 @@
 import React from "react"
+import Header from "../componente/Header";
 
 const Home = () => {
     return (
         <div>
-            Home
+            <Header />
         </div>
     )
 }
