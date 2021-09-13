ef2c20eff84c4a25295d11735cd94a96bc5ba368
mrcomputer2018 <45561350+mrcomputer2018@users.noreply.github.com>
2021-09-13 09:31:01 -0300

    pagina mensagem

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,7 +6,46 @@ const Home = () => {
     return (
         <div className="home">
             <Header />
-            <h2>Mensagem</h2>
+            <div>
+                <div>
+                    <h2>Mensagens</h2>
+                </div>
+                <div>
+                    <button>Pesquisar</button>
+                    <button>Nova mensagem</button>
+                </div>
+            </div>
+            
+            <form>
+                <div className="form-group">
+                    <label htmlFor="select-gatilho">Gatilho:</label>
+                    <select name="select-gatilho">
+                        <option value="valor1"selected></option>
+                        <option value="valor2">Tios</option>
+                        <option value="valor3">Avós</option>
+                        <option value="valor3">Padrinhos</option>
+                    </select>
+                </div>
+                <div className="form-group">
+                <label htmlFor="select-canal">Canal:</label>
+                    <select name="select-canal">
+                        <option value="valor1"selected></option>
+                        <option value="valor2">Tios</option>
+                        <option value="valor3">Avós</option>
+                        <option value="valor3">Padrinhos</option>
+                    </select>
+                </div>
+                <div className="form-group">
+                    <label htmlFor="select-timer">Timer:</label>
+                    <select name="select-timer">
+                        <option value="valor1"selected></option>
+                        <option value="valor2">Tios</option>
+                        <option value="valor3">Avós</option>
+                        <option value="valor3">Padrinhos</option>
+                    </select>
+                </div>
+            </form>
+            
         </div>
     )
 }
